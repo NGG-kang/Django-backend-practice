70229d4c1f9a6691590a1435c8e1b156a61f0562
NGG-Kang <skarndrkd1222@gmail.com>
2021-01-21 17:33:21 +0900

    1/21 django board practice

--- DjangoPractice/pcmain/pcsub/urls.py
+++ DjangoPractice/pcmain/pcsub/urls.py
@@ -6,5 +6,7 @@ from . import views
 app_name = 'pcsub'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-
+    path('write/', views.write.as_view(),name='write'),
+    path('write/writeBoard/', views.writeBoard, name='writeBoard'),
+    path('detail/<int:pk>/', views.Detail, name='detail'),
 ]
--- DjangoPractice/pcmain/pcsub/views.py
+++ DjangoPractice/pcmain/pcsub/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import generic
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.views import generic, View
 from django.utils import timezone
 from .models import Board
+import json
+
 
 class IndexView(generic.ListView):
     template_name = 'index.html'
@@ -12,3 +14,26 @@ class IndexView(generic.ListView):
         return Board.objects.filter(
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
+
+
+class write(generic.TemplateView):
+    template_name = 'write.html'
+
+
+class Detail(generic.View):
+    template_name = 'detail.html'
+    context_object_name = 'board_list'
+
+    def get_queryset(self):
+        return Board.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+def writeBoard(request):
+    title = request.POST.get('title')
+    context = request.POST.get('context')
+    pub_date = timezone.now()
+    board = Board(title=title, context=context, pub_date=pub_date)
+    board.save()
+    return HttpResponseRedirect('/pcsub')
